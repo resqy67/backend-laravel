7bd3bbb2a57c604458c44c93bc43e6a6ae343ffd
Resqy Hedy Kurniawan <resqyhedy@gmail.com>
2024-07-17 08:46:38 +0800

    feat: Improve error handling in SendBookAddedNotification

--- app/Listeners/SendBookAddedNotification.php
+++ app/Listeners/SendBookAddedNotification.php
@@ -7,6 +7,7 @@ use App\Notifications\PushNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
 use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Laravel\Firebase\Facades\Firebase;
 
@@ -32,12 +33,22 @@ class SendBookAddedNotification
         foreach ($users as $user) {
             if ($user->token_fcm) {
                 // $user->notify(new PushNotification('Book Added', 'A new book has been added. Check it out now! 📚' . $event->book->title));
-                $message = CloudMessage::withTarget('token', $user->token_fcm)
-                    ->withNotification([
-                        'title' => 'Book Added',
-                        'body' => 'A new book has been added. Check it out now! 📚'
-                    ]);
-                $messaging->send($message);
+                try {
+                    $message = CloudMessage::withTarget('token', $user->token_fcm)
+                        ->withNotification([
+                            'title' => 'Book Added',
+                            'body' => 'A new book has been added. Check it out now! 📚'
+                        ]);
+                    $messaging->send($message);
+                } catch (\Kreait\Firebase\Exception\MessagingException $e) {
+                    // Handle exception
+                    Log::error("FCM token for user {$user->id} is invalid: {$e->getMessage()}");
+                    $user->token_fcm = null;
+                    $user->save();
+                } catch (\Kreait\Firebase\Exception\FirebaseException $e) {
+                    // Handle exception
+                    Log::error("An error occurred while sending FCM notification to user {$user->id}: {$e->getMessage()}");
+                }
             }
         }
     }
