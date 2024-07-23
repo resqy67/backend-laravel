9f031693d2b6024aefdff79f65ba0221270e714f
Resqy Hedy Kurniawan <resqyhedy@gmail.com>
2024-07-23 21:24:41 +0800

    feat: Send notification when a book is returned automatically

--- app/Console/Commands/AutoReturnBooks.php
+++ app/Console/Commands/AutoReturnBooks.php
@@ -5,6 +5,8 @@ namespace App\Console\Commands;
 use App\Models\Book;
 use App\Models\Loan;
 use Illuminate\Console\Command;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Laravel\Firebase\Facades\Firebase;
 
 class AutoReturnBooks extends Command
 {
@@ -36,6 +38,7 @@ class AutoReturnBooks extends Command
         $returnCount = 0;
 
         foreach ($loans as $loan) {
+            $this->sendNotification($loan);
             $loan->delete();
 
             $book = Book::where('uuid', $loan->book_uuid)->first();
@@ -47,5 +50,31 @@ class AutoReturnBooks extends Command
         }
 
         $this->info('Books returned successfully. Total books returned: ' . $returnCount . ' books. ' . $today);
+    }
+    public function sendNotification($loan): void
+    {
+        $messaging = Firebase::messaging();
+        $user = $loan->user;
+        if ($user->token_fcm) {
+            // Log::info("Sending FCM notification to user {$user->id} with token {$user->token_fcm}");
+            try {
+                $message = CloudMessage::withTarget('token', $user->token_fcm)
+                    ->withNotification([
+                        'title' => 'Book Returned',
+                        'body' => 'Your book has been returned successfully'
+                    ]);
+                $messaging->send($message);
+            } catch (\Kreait\Firebase\Exception\MessagingException $e) {
+                // Handle exception
+                // Log::error("FCM token for user {$user->id} is invalid: {$e->getMessage()}");
+                $user->token_fcm = null;
+                $user->save();
+            } catch (\Kreait\Firebase\Exception\FirebaseException $e) {
+                // Handle exception
+                // Log::error("An error occurred while sending FCM notification to user {$user->id}: {$e->getMessage()}");
+            }
+        } else {
+            // Log::info("FCM token for user {$user->id} is not set");
+        }
     }
 }
