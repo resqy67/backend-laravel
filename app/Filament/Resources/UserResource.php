bb95daf988ad1c6f6c411c2bd9a6ca515ce42334
Resqy Hedy Kurniawan <resqyhedy@gmail.com>
2024-08-28 23:36:48 +0800

    feat: Add class and description select fields to UserResource form

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -40,12 +40,45 @@ class UserResource extends Resource
                     ->image()
                     ->directory('users/avatars')
                     ->visibility('public'),
-                Forms\Components\TextInput::make('class')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('description')
-                    ->required()
-                    ->maxLength(255),
+                Forms\Components\Select::make('class')
+                    ->options([
+                        'X TKJ 1' => 'X TKJ 1',
+                        'X TKJ 2' => 'X TKJ 2',
+                        'X MPLB 1' => 'X MPLB 1',
+                        'X MPLB 2' => 'X MPLB 2',
+                        'X PPLG' => 'X PPLG',
+                        'X DKV' => 'X DKV',
+                        'X AKL' => 'X AKL',
+                        'XI TKJ' => 'XI TKJ',
+                        'XI MPLB 1' => 'XI MPLB 1',
+                        'XI MPLB 2' => 'XI MPLB 2',
+                        'XI PPLG' => 'XI PPLG',
+                        'XI DKV 1' => 'XI DKV 1',
+                        'XI DKV 2' => 'XI DKV 2',
+                        'XI AKL' => 'XI AKL',
+                        'XII TKJ 1' => 'XII TKJ 1',
+                        'XII TKJ 2' => 'XII TKJ 2',
+                        'XII MPLB 1' => 'XII MPLB 1',
+                        'XII MPLB 2' => 'XII MPLB 2',
+                        'XII PPLG' => 'XII PPLG',
+                        'XII DKV 1' => 'XII DKV 1',
+                        'XII DKV 2' => 'XII DKV 2',
+                        'XII AKL' => 'XII AKL',
+                    ])
+                    ->required(),
+                // Forms\Components\TextInput::make('class')
+                //     ->required()
+                //     ->maxLength(255),
+                Forms\Components\Select::make('description')
+                    ->options([
+                        'Seorang Siswa' => 'Seorang Siswa',
+                        'Seorang Guru' => 'Seorang Guru',
+                        'Seorang Karyawan' => 'Seorang Karyawan',
+                    ])
+                    ->required(),
+                // Forms\Components\TextInput::make('description')
+                //     ->required()
+                //     ->maxLength(255),
                 Forms\Components\TextInput::make('password')
                     ->password()
                     ->required()
