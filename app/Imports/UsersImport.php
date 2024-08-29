a8ce45529081c16286a166175801a5bd862a5dea
Resqy Hedy Kurniawan <resqyhedy@gmail.com>
2024-08-29 08:32:54 +0800

    chore: Update UsersImport to use associative array keys for model creation

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -3,9 +3,11 @@
 namespace App\Imports;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class UsersImport implements ToModel
+class UsersImport implements ToModel, WithHeadingRow
 {
     /**
      * @param array $row
@@ -15,12 +17,18 @@ class UsersImport implements ToModel
     public function model(array $row)
     {
         return new User([
-            'name' => $row[0],
-            'email' => $row[1],
-            'nisn' => $row[2],
-            'class' => $row[3],
-            'description' => $row[4],
-            'password' => $row[5],
+            // 'name' => $row[0],
+            // 'email' => $row[1],
+            // 'nisn' => $row[2],
+            // 'class' => $row[3],
+            // 'description' => $row[4],
+            // 'password' => $row[5],
+            'name' => $row['name'],
+            'email' => $row['email'],
+            'nisn' => $row['nisn'],
+            'class' => $row['class'],
+            'description' => $row['description'],
+            'password' => Hash::make($row['password']),
         ]);
     }
 }
