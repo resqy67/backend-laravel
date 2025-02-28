d45098ec1a7c9a5ccc4b68905613edc65873aee5
Resqy Hedy Kurniawan <resqyhedy@gmail.com>
2025-02-28 23:35:39 +0800

    feat: Add privacy policy and terms and conditions pages

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/privacy', function() {
+    return view('privacy');
+});
+
+Route::get('/terms-and-condition', function() {
+    return view('terms-and-conditions');
+});
+
 Route::domain('/')->group(function () {
     Scramble::registerUiRoute('api');
     Scramble::registerJsonSpecificationRoute('api.json');
